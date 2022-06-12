cdea6f87b40be8e0166f263ca0d519b2747f0ff0
Sarat Chandra <me@saratchandra.in>
2022-06-12 22:27:55 +0530

    fix: Fix installation path.

--- lib/utils.bash
+++ lib/utils.bash
@@ -68,7 +68,7 @@ install_version() {
 
   (
     mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path/bin"
 
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
